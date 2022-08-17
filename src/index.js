5f1ea5745b9ecafe9820d982308bbd6577cb40fb
Nnamdi <nnamdiokoroafor567@gmail.com>
2022-08-17 11:51:56 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './assets/medium.css';
+import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store, history } from './redux/store';
+import { getUser } from './redux/actions/actions'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+if(localStorage.Auth) {
+    // update localstorage
+    store.dispatch({type: 'SET_USER', user: JSON.parse(localStorage.Auth)})
+    var _id = JSON.parse(localStorage.Auth)._id
+    getUser(_id).then((res) => {
+        store.dispatch({type: 'SET_USER', user: res})
+    })
+}
+
+ReactDOM.render((
+  <Provider store={store}>
+      <ConnectedRouter history={history}>
+          <Switch>
+              <Route path="/" component={App} />
+          </Switch>
+      </ConnectedRouter>
+  </Provider>
+), document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
